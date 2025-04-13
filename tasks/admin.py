d7deed40d921237931c9e48f96dddbef761966c4
mshel <maryna.shelenkova@gmx.de>
2025-04-13 20:55:06 +0200

    Добавлены модели Task и SubTask, реализована админка

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,31 +1,39 @@
-# from django.contrib import admin
-# from .models import Task, SubTask, Category
-#
-# admin.site.register(Task)
-# admin.site.register(SubTask)
-# admin.site.register(Category)
-
 from django.contrib import admin
-from .models import Task, SubTask, Category
+from django.db.models import QuerySet
 
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'created_at', 'deadline')
-    search_fields = ('title', 'description')
-    list_filter = ('status', 'created_at', 'categories')
-    ordering = ['-created_at']
+from tasks.models import Task, SubTask, Category
 
+
+class SubTaskInline(admin.StackedInline):
+    model = SubTask
+    extra = 1
+
+@admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'task', 'status', 'created_at', 'deadline')
-    search_fields = ('title', 'description')
-    list_filter = ('status', 'created_at')
-    ordering = ['-created_at']
+    actions = ['set_subtask_status_to_done']
+    list_display = ('task_title', 'title', 'description', 'deadline', 'status')
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-    ordering = ['name']
+    def task_title(self, obj):
+        return obj.task.title
 
-admin.site.register(Task, TaskAdmin)
-admin.site.register(SubTask, SubTaskAdmin)
-admin.site.register(Category, CategoryAdmin)
+    def set_subtask_status_to_done(self, request, queryset: QuerySet):
+        updated = queryset.update(status="Done")
+        self.message_user(request, f"Статус обновлен на 'Done' для {updated} подзадач.")
+
+    set_subtask_status_to_done.short_description = "Обновить статусы на Done"
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [SubTaskInline]
+    list_display = ('short_title', 'description', 'categories_names', 'deadline', 'status')
+    list_filter = ('title', 'categories__name', 'deadline', 'status')
+    list_per_page = 5
+
+    def short_title(self, obj: Task):
+        return f"{obj.title[:10]}..." if len(obj.title) > 10 else obj.title
+
+    def categories_names(self, obj: Task):
+        return ", ".join([cat.name for cat in obj.categories.all()])
+
+admin.site.register(Category)
 
